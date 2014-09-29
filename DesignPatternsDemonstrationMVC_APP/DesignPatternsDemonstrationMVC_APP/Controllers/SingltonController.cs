61d167b4119d78b49e312f2a92234208d5fce2a2
Mare - Marko Radosevic <mareee901@gmail.com>
2014-09-29 15:45:54 +0200

    Singlton pattern added

---
+++ DesignPatternsDemonstrationMVC_APP/DesignPatternsDemonstrationMVC_APP/Controllers/SingltonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DesignPatternsDemonstrationMVC_APP.pattern_classes.SingltonPattern;
+
+
+namespace DesignPatternsDemonstrationMVC_APP.Controllers
+{
+    public class SingltonController : Controller
+    {
+        // GET: Singlton
+        public ActionResult Index()
+        {
+
+
+            President pr = President.Instance();
+            pr.SetPresidentName("New prezident name");
+            pr.SetPresidentAge(44);
+
+            President pr2 = President.Instance();
+            pr2.SetPresidentAge(23);
+
+            return View();
+        }
+    }
+}
---
+++ DesignPatternsDemonstrationMVC_APP/DesignPatternsDemonstrationMVC_APP/pattern_classes/SingltonPattern/SingltonPattern.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesignPatternsDemonstrationMVC_APP.pattern_classes.SingltonPattern
+{
+
+
+    public class President
+    {
+        public string name;
+        public int age;
+
+        private static President president = null;
+
+
+
+        private President(){}
+
+        public static President Instance()
+        {
+            if (president == null) 
+            {
+                president = new President();
+            }
+            return president;
+        }
+
+        public void SetPresidentName(string newName) 
+        {
+            president.name = newName;
+        }
+
+        public void SetPresidentAge(int newAge) 
+        {
+            president.age = newAge;
+        }
+
+        public string GetPresidentName() 
+        {
+            return president.name;
+        }
+
+    }
+
+
+}
