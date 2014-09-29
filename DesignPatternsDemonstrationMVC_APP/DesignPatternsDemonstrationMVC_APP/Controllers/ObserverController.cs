37211fa246ecb380fbb51d7030a94f50e7bc9649
Mare - Marko Radosevic <mareee901@gmail.com>
2014-09-29 16:56:28 +0200

    Observer pattern added

---
+++ DesignPatternsDemonstrationMVC_APP/DesignPatternsDemonstrationMVC_APP/Controllers/ObserverController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DesignPatternsDemonstrationMVC_APP.pattern_classes.ObserverPattern;
+
+namespace DesignPatternsDemonstrationMVC_APP.Controllers
+{
+    public class ObserverController : Controller
+    {
+        // GET: Observer
+        public ActionResult Index()
+        {
+
+            Company company = new Company();
+
+            DepartmentManager dm1 = new DepartmentManager();
+            DepartmentManager dm2 = new DepartmentManager();
+
+            Developer dev1 = new Developer();
+            Developer dev2 = new Developer();
+
+            company.AddEmploy(dm1);
+            company.AddEmploy(dm2);
+            company.AddEmploy(dev1);
+            company.AddEmploy(dev2);
+
+            company.NotifyManagers();
+
+
+
+
+            return View();
+        }
+    }
+}
---
+++ DesignPatternsDemonstrationMVC_APP/DesignPatternsDemonstrationMVC_APP/pattern_classes/ObserverPattern/ObserverPattern.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesignPatternsDemonstrationMVC_APP.pattern_classes.ObserverPattern
+{
+    
+    public interface IEmployee
+    {
+        void Notify();
+    }
+
+    public class DepartmentManager : IEmployee
+    {
+       
+        public void Notify()
+        {
+            Console.WriteLine("Department manager is informed about change");
+        }
+
+
+    }
+
+    public class Developer : IEmployee 
+    {
+        public void Notify()
+        {
+            Console.WriteLine("Developer is informed about change");
+        }
+    }
+
+
+
+    public class Company 
+    {
+        private List<IEmployee> employeeList = new List<IEmployee>();
+
+        public void AddEmploy(IEmployee emp) 
+        {
+            employeeList.Add(emp);
+        }
+
+        public void RemoveEmploye(IEmployee emp) 
+        {
+            employeeList.Remove(emp);
+        }
+
+        public void NotifyManagers() 
+        {
+            foreach(var employee in employeeList)
+            {
+                if(employee.GetType() == typeof(DepartmentManager))
+                {
+                    employee.Notify();
+                }
+            }
+        }
+
+        public void NotifyDevelopers()
+        {
+            foreach (var employee in employeeList)
+            {
+                if (employee.GetType() == typeof(Developer))
+                {
+                    employee.Notify();
+                }
+            }
+        }
+
+        public void NotifyAll()
+        {
+            foreach (var employee in employeeList)
+            {
+                 employee.Notify();
+            }
+        }
+
+
+    }
+
+
+}
